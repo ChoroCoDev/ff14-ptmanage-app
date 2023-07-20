f53421d9d6622658d354ffa5846c5bbbb3cfaaa0
ChoroCoDev <choroco.dev@gmail.com>
2023-07-21 02:34:52 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['img2.finalfantasyxiv.com'],
+  },
+}
 
 module.exports = nextConfig
